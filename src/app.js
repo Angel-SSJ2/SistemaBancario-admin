b9daf3ef4966f0e0fe58788d145a24d6e5ea645a
Angel-SSJ2 <arodriguez-2021698@kinal.edu.gt>
2026-02-12 00:45:35 -0600

    login y registro de administrador con JWT completado

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 
@@ -9,5 +10,9 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 
+app.use('/banco/v1/auth', authRoutes);
+
+module.exports = app;
 // Exportamos la configuración
 module.exports = app; 
+
