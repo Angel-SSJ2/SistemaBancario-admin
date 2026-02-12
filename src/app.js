745dbe0313df6cde9484fcd8c7183f254c296746
Angel-SSJ2 <arodriguez-2021698@kinal.edu.gt>
2026-02-11 22:48:23 -0600

    Sprint 1 - server y DB

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+// Exportamos la configuración
+module.exports = app; 
