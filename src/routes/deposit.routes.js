01ad4e1cbd052768457f1849f930c3815341b322
Angel-SSJ2 <arodriguez-2021698@kinal.edu.gt>
2026-03-03 10:34:15 -0600

    Configuracion de Carpetas

--- src/routes/deposit.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { getHistory } = require('../controllers/deposit.controller');
-const { validateJWT } = require('../middlewares/validate-jwt');
-
-const router = Router();
-
-router.get('/history', validateJWT, getHistory);
-
-module.exports = router;
