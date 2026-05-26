417b78eb0e7c081f4fadb62d416d846e4c3b3f00
Mcho2021304 <mcho-2021304@kinal.edu.gt>
2026-05-26 11:56:29 -0600

    chore: agregar punto de entrada y dependencias del proyecto

---
+++ index.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+import { initServer } from './configs/app.js';
+
+dotenv.config();
+
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err);
+    process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+    process.exit(1);
+});
+
+console.log('Iniciando servidor Admin...');
+initServer();
